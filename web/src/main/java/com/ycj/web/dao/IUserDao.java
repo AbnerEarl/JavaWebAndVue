2104ed9dcef6b3c0033532463841fa8d3c771079
YouAreOnlyOne <1320259466@qq.com>
2019-11-29 11:11:13 +0800

    jdbc方式操作数据

---
+++ web/src/main/java/com/ycj/web/dao/IUserDao.java
@@ -0,0 +1,11 @@
+package com.ycj.web.dao;
+
+import java.util.ArrayList;
+import java.util.Map;
+
+public interface IUserDao {
+
+    public ArrayList<Map<String, Object>> getUser(String username, String password);
+
+    public int insertUser(String username, String password);
+}
